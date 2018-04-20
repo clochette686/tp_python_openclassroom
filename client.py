d61b122ab6688cd04745eeef497de1126ab2d67f
Christelle SALA <csala@sii.fr>
2018-04-20 16:00:08 +0200

    nettoyage du code du client

--- client.py
+++ client.py
@@ -4,14 +4,7 @@ from threading import RLock
 import re
 import time
 
-hote = "localhost"
-port = 12800
 
-
-
-verrou_cmd_utilisateur = RLock()
-
-connection_active = True
 
 class Partie_envoie_messages_serveur(Thread):
 
@@ -20,6 +13,7 @@ class Partie_envoie_messages_serveur(Thread):
         self.serveur = connexion_serveur
         self.message_a_envoyer = []
         self.verrou_msg_a_envoyer = RLock()
+        self.connection_active = True
 
     def set_message(self, message):
         with self.verrou_msg_a_envoyer:
@@ -27,7 +21,7 @@ class Partie_envoie_messages_serveur(Thread):
             self.message_a_envoyer.append(message.encode())
     
     def run(self):
-        while connection_active:
+        while self.connection_active:
             print("envoie de messages au serveur\n")
             with self.verrou_msg_a_envoyer:
                 while len(self.message_a_envoyer) != 0:
@@ -43,6 +37,7 @@ class Partie_reception_messages_serveur(Thread):
         self.serveur = connexion_serveur
         self.messages_recus = []
         self.verrou_msg_a_lire = RLock()
+        self.connection_active = True
 
     def get_messages(self):
         with self.verrou_msg_a_lire:
@@ -53,7 +48,7 @@ class Partie_reception_messages_serveur(Thread):
             return messages
 
     def run(self):
-        while connection_active:
+        while self.connection_active:
             print("receptionner les messages")
             with self.verrou_msg_a_lire:
                 message = self.serveur.recv(1024)
@@ -66,15 +61,17 @@ class Partie_lecture_entree_clavier(Thread):
     def __init__(self):
         Thread.__init__(self)
         self.saisie_utilisateur = []
+        self.verrou_cmd_utilisateur = RLock()
+        self.connection_active = True
 
     def get_commandes(self):
-        with verrou_cmd_utilisateur:
+        with self.verrou_cmd_utilisateur:
             liste_commandes = list(self.saisie_utilisateur)
             self.saisie_utilisateur = []
             return liste_commandes
 
     def get_first_commande(self):
-        with verrou_cmd_utilisateur:
+        with self.verrou_cmd_utilisateur:
             if len(self.saisie_utilisateur) > 0:
                 commande = self.saisie_utilisateur.pop(0)
                 return commande
@@ -82,31 +79,23 @@ class Partie_lecture_entree_clavier(Thread):
                 return None
 
     def run(self):
-        while connection_active:
-            with verrou_cmd_utilisateur:
+        while self.connection_active:
+            with self.verrou_cmd_utilisateur:
                 commande = input()
                 self.saisie_utilisateur.append(commande)
             time.sleep(100)    
 
 
-connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connexion_avec_serveur.connect((hote, port))
-print("Connexion établie avec le serveur sur le port {}".format(port))
+def connexion_au_serveur():
+    hote = "localhost"
+    port = 12800    
+    connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connexion_avec_serveur.connect((hote, port))
+    print("Connexion établie avec le serveur sur le port {}".format(port))
+    return connexion_avec_serveur
 
-#demarrer les thread de communication
-#thread_input_clavier = Partie_lecture_entree_clavier()
-thread_envoi_serveur = Partie_envoie_messages_serveur(connexion_avec_serveur)
-thread_recep_serveur = Partie_reception_messages_serveur(connexion_avec_serveur)
 
-
-#thread_input_clavier.start()
-thread_envoi_serveur.start()
-thread_recep_serveur.start()
-
-partie_commencee = False
-
-#phase d'attente du debut de la partie
-while not partie_commencee:
+def traitement_messages_serveur(thread_recep_serveur):
     #on recupere et affiche tous les messages du serveur
     messages = thread_recep_serveur.get_messages()
     if len(messages) > 0:
@@ -117,8 +106,9 @@ while not partie_commencee:
             print(message + "\n")
         #apres chaque nouveau message du serveur, on rappelle au joueur la commande pour lancer la partie
         if not partie_commencee:
-            print("Appuyez sur C pour commencer la partie \n")
+            print("Appuyez sur C pour commencer la partie \n")    
 
+def traitement_input_client(thread_envoi_serveur, partie_commencee):
     #on regarde parmi les saisies clavier du joueur si il n'aurait pas demandé à démarrer la partie
     #commandes = thread_input_clavier.get_commandes()
     commande = input()
@@ -127,12 +117,19 @@ while not partie_commencee:
     if commande.rstrip().upper() == 'C':
         thread_envoi_serveur.set_message(commande)
         partie_commencee = True
+    return partie_commencee    
 
-#demarrage de la partie            
-partie_terminee = False
+def attente_demarrage_partie(thread_envoi_serveur, thread_recep_serveur):
+    partie_commencee = False
 
-#on boucle tant que personne n'a gagné la partie
-while not partie_terminee:
+    #phase d'attente du debut de la partie
+    while not partie_commencee:
+        traitement_messages_serveur(thread_recep_serveur)
+        partie_commencee = traitement_input_client(thread_envoi_serveur, partie_commencee)
+   
+
+
+def attendre_son_tour(thread_recep_serveur, thread_envoi_serveur, partie_terminee):
     mon_tour = False
     #en attendant son tour de jeu, on affiche toutes les infos recues par le serveur
     while not mon_tour and not partie_terminee:
@@ -144,22 +141,61 @@ while not partie_terminee:
             #verifie si le serveur nous signale que la partie est terminée    
             if re.search(r"gagné", message):
                 partie_terminee = True
-                connection_active = False #condition pour arreter les thread de communication
-            print(message + "\n")    
+                thread_envoi_serveur.connection_active = False #condition pour arreter les thread de communication
+                thread_recep_serveur.connection_active = False #condition pour arreter les thread de communication
+            print(message + "\n")
+    return (mon_tour, partie_terminee)        
 
-    #c'est mon tour
-    if mon_tour:
-        commande_valide = ""
-        while commande_valide == "":
-            print("Veuillez entrer votre prochain mouvement")
-            #commande = thread_input_clavier.get_first_commande()
-            commande = input()
-            if commande.rstrip().upper() in ["N","S","E","W"]:
-                commande_valide = commande
-                
-        #le joueur a saisi une commande valide, on l'envoie au serveur
-        thread_envoi_serveur.set_message(commande_valide)
+def jouer_son_tour(thread_envoi_serveur):
+    commande_valide = ""
+    while commande_valide == "":
+        print("Veuillez entrer votre prochain mouvement")
+        #commande = thread_input_clavier.get_first_commande()
+        commande = input()
+        if commande.rstrip().upper() in ["N","S","E","W"]:
+            commande_valide = commande
+            
+    #le joueur a saisi une commande valide, on l'envoie au serveur
+    thread_envoi_serveur.set_message(commande_valide)    
+
+def deroulement_tours_de_jeu(thread_recep_serveur, thread_envoi_serveur):
+    #demarrage de la partie            
+    partie_terminee = False
+
+    #on boucle tant que personne n'a gagné la partie
+    while not partie_terminee:
+
+        (mon_tour, partie_terminee) = attendre_son_tour(thread_recep_serveur, thread_envoi_serveur, partie_terminee)
+
+        #c'est mon tour
+        if mon_tour and not partie_terminee:
+            jouer_son_tour(thread_envoi_serveur)
+
+def fermeture_connexion_serveur(connexion_avec_serveur):
+    print("Fermeture de la connexion")
+    connexion_avec_serveur.close()    
+
+def client_main():
+    connexion_avec_serveur = connexion_au_serveur()
 
 
-print("Fermeture de la connexion")
-connexion_avec_serveur.close()
+    #demarrer les thread de communication
+    #thread_input_clavier = Partie_lecture_entree_clavier()
+    thread_envoi_serveur = Partie_envoie_messages_serveur(connexion_avec_serveur)
+    thread_recep_serveur = Partie_reception_messages_serveur(connexion_avec_serveur)
+
+    #thread_input_clavier.start()
+    thread_envoi_serveur.start()
+    thread_recep_serveur.start()
+
+    attente_demarrage_partie(thread_envoi_serveur, thread_recep_serveur)
+
+    deroulement_tours_de_jeu(thread_recep_serveur, thread_envoi_serveur)
+
+    fermeture_connexion_serveur(connexion_avec_serveur)
+
+client_main()    
+
+
+
+
